27b1af8a7032f1af414c86109900b631ea76e4c6
ramiro33 <107439581+ramiro33@users.noreply.github.com>
2025-09-18 22:55:06 -0300

    Update page.tsx

--- app/pedidos/page.tsx
+++ app/pedidos/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Pedidos() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Pedidos</h1>
+      <p>Contenido de la página de pedidos.</p>
+    </div>
+  );
+}
