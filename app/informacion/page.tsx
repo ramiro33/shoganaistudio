4ace32f7da966d41bcd53696da20a0917b99d70a
ramiro33 <107439581+ramiro33@users.noreply.github.com>
2025-09-18 22:54:10 -0300

    Update page.tsx

--- app/informacion/page.tsx
+++ app/informacion/page.tsx
@@ -1,1 +1,8 @@
-
+export default function Informacion() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Información</h1>
+      <p>Contenido de la página de información.</p>
+    </div>
+  );
+}
